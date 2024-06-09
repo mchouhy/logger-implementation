1c96fd56b26b3ae9507267fc91e88fe90d588241
mchouhy <marianochouhy@gmail.com>
2024-06-10 00:03:19 +0200

    fix level config error warn > warning

--- src/controllers/loggerController.js
+++ src/controllers/loggerController.js
@@ -2,7 +2,7 @@ export const loggerController = (request, response) => {
   request.logger.debug("Mensaje de Debug");
   request.logger.http("Mensaje de HTTP");
   request.logger.info("Mensaje de Info");
-  request.logger.warn("Mensaje de Warning");
+  request.logger.warning("Mensaje de Warning");
   request.logger.error("Mensaje de Error");
   request.logger.fatal("Mensaje de Error Fatal");
   response.send("Logs de error generados con éxito.");
